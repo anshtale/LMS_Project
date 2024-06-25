b4eaf9b384f4710265201c2f054bc5ef5abb0abe
Ansh Tale <anshtale2004@gmail.com>
2024-06-26 01:24:25 +0530

    added Chapter ,MuxData ,UserProgress,Purchase,StripeCustomer tables in primsa schema

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
@@ -0,0 +1,127 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
+import { cn } from "@/lib/utils"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Chapter, Course } from "@prisma/client"
+import axios from "axios"
+import { Pencil, PlusCircle } from "lucide-react"
+import { useRouter } from "next/navigation"
+import { title } from "process"
+import { useState } from "react"
+import { useForm } from "react-hook-form"
+import toast from "react-hot-toast"
+import { z } from "zod"
+
+interface ChaptersFormProps{
+    intialData:Course & {chapters : Chapter[]}
+    courseId:string
+}
+
+const formSchema = z.object({
+    title:z.string().min(1),
+})
+const ChaptersForm = ({
+    intialData,
+    courseId
+}:ChaptersFormProps) => {
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            title: "",
+        }
+    });
+    const router = useRouter();
+    const [isUpdating,setIsUpdating] = useState(false);
+    const [isCreating,setIsCreating] = useState(false);
+
+
+    const toggleCreating= ()=> {
+        setIsCreating((isCreating)=>(!isCreating));
+    }
+
+    const { isSubmitting, isValid } = form.formState;
+    const onSubmit = async (values : z.infer<typeof formSchema>)=>{
+        try{
+            await axios.post(`/api/courses/${courseId}/chapters`,values);
+            toast.success("Chapter Created!")
+            toggleCreating();
+            router.refresh();
+
+        }catch{
+            toast.error("Something went wrong!");
+        }
+    }
+
+    return (
+        <div className="mt-6 bg-slate-100 rounded-md p-4">
+            <div className="font-medium flex items-center justify-between">
+                Course Chapters
+            
+                <Button onClick={toggleCreating} value="ghost">
+                    {isCreating ? (
+                        <>Cancel</>
+                    ):( <>
+                        <PlusCircle className="h-4 w-4 mr-2"/>
+                        Add a Chapter
+                        </>
+                    )}
+                    
+                </Button>
+            </div>
+    
+            {isCreating && (
+                <Form {...form}>
+                    <form 
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className="space-y-4 mt-4"
+                    >
+                        <FormField
+                            control={form.control}
+                            name = "title"
+                            render={({field}) =>(
+                                <FormItem>
+                                    <FormControl>
+                                        <Input
+                                            disabled={isSubmitting}
+                                            placeholder="e.g. 'Introduction to the course'"
+                                            {...field}/>
+                                    </FormControl>
+                                    <FormMessage/>
+                                </FormItem>
+                            )}>
+
+                        </FormField>
+
+                        <Button
+                            disabled = {isSubmitting || !isValid}
+                            type="submit">
+                            Create
+                        </Button>
+
+                    </form>
+                </Form>
+            )}
+            {!isCreating && (
+                <div className={cn(
+                    "text-sm mt-2",
+                    !intialData.chapters.length && "text-slate-500 italic"
+                )}>
+                   {!intialData.chapters.length && "No chapters"}
+                   {/*TODO: Add a list of chaptes*/ }
+                </div>
+            )}
+            {!isCreating && (
+                <p>
+                    Drag & Drop to reorder the chapters
+                </p>
+            )}
+        </div>
+    )
+}
+
+export default ChaptersForm;
--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -9,6 +9,7 @@ import ImageForm from "./_components/image-form"
 import CategoryForm from "./_components/category-form"
 import PriceForm from "./_components/price-form"
 import AttachmentForm from "./_components/attachment-form"
+import ChaptersForm from "./_components/chapters-form"
 
 
 
@@ -24,12 +25,18 @@ const CourseIdPage = async ({ params }:{
     }
     const course = await db.course.findUnique({
         where:{
+            userId,
             id:params.courseId
         },
         include:{
             attachments:{
                 orderBy:{
                     createdAt:"desc",
+                }
+            },
+            chapters:{
+                orderBy:{
+                  position:"asc"
                 }
             }
         }
@@ -48,7 +55,8 @@ const CourseIdPage = async ({ params }:{
         course.description,
         course.imageUrl,
         course.price,
-        course.categoryId
+        course.categoryId,
+        course.chapters.some(chapter=> chapter.isPublished),
     ];
 
     const totalFields = requiredFields.length;
@@ -107,9 +115,10 @@ const CourseIdPage = async ({ params }:{
                             </h2>
                         </div>
                     </div>
-                    <div>
-                        TODO:Chapters
-                    </div>
+                    <ChaptersForm
+                        intialData = {course}
+                        courseId = {course.id}
+                    />
                     <div>
                         <div className="flex items-center gap-x-2">
                             <IconBadge icon={CircleDollarSign}/>
